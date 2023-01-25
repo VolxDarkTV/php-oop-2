e57b99f140aa5d9d3cad726154f61aa7c462cc75
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-25 16:47:14 +0100

    add price

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ include("toy.php");
 
 class Products{
     private $name;
+    private $price;
     private $description;
 
-    public function __construct($name, $description){
+    public function __construct($name, $price, $description){
         $this -> setName($name);
+        $this -> setPrice($price);
         $this -> setDescription($description);
     }
 
@@ -19,6 +21,13 @@ class Products{
     }
     public function setName($name){
         return $this -> name = $name;
+    }
+    // Price
+    public function getPrice(){
+        return $this -> price;
+    }
+    public function setPrice($price){
+        return $this -> price = $price;
     }
     // Description
     public function getDescription(){
@@ -30,11 +39,12 @@ class Products{
 }
 
 $products = [
-    new Products("Osso di Gomma", "giocattolo adatto a tutti i cuccioli"),
-    new Products("Osso di plastica", "giocattolo adatto a i cuccioli di almeno 1 anno"),
+    new Products("Osso di Gomma", 50 . "$", "giocattolo adatto a tutti i cuccioli"),
+    new Products("Osso di plastica", 40 . "$", "giocattolo adatto a i cuccioli di almeno 1 anno"),
 ];
 foreach($products as $product){
     echo "<span>" . $product -> getName() . "</span> <br>"
+    . "<span>" . $product -> getPrice() . "</span> <br>"
     . "<span>" . $product -> getDescription() . "</span> <br>";
 }
 
