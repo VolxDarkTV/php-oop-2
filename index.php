e6773119a56a8a2f92a8633a23df5d083da22d44
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-25 14:50:37 +0100

    add Dog class and Objects

--- index.php
+++ index.php
@@ -1,31 +1,50 @@
 <?php
 class Dog {
     private $category;
-    private $date_of_birth;
+    private $dateOfBirth;
 
-    public function __construct($category, $date_of_birth){
+    public function __construct($category, $dateOfBirth){
         $this -> setCategory($category);
-        $this -> setCategory($date_of_birth);
+        $this -> setDateOfBirth($dateOfBirth);
 
     }
 
+    // Category
     public function getCategory(){
         return $this -> category;
     }
-    public function setCategory(){
-        return $this -> category;
+    public function setCategory($category){
+        return $this -> category = $category;
     }
 
+    // DateOfBirth
+    public function getDateOfBirth(){
+        return $this -> dateOfBirth;
+    }
+    public function setDateOfBirth($dateOfBirth){
+        return $this -> dateOfBirth = $dateOfBirth;
+    }
+
+
 }
 
-class Cat{
+// class Food{
+//     private $name;
+//     private $ingredients;
+//     private $expiration_date;
 
+    
+// }
+
+$dogs = [
+    new Dog("Alano", "27-05-2021"), 
+    new Dog("Corso", "27-05-2009"), 
+    new Dog("Labrador", "27-05-2011"), 
+    new Dog("Maremmano", "27-05-2019")
+];
+
+foreach ($dogs as $dog){
+    echo "<h1>" . $dog -> getCategory() . "</h1>" . "<span>" . $dog -> getDateOfBirth() . "</span>";
 }
-
-class Toys{
-
-}
-
-$dog = new Dog("Alano", "27-05-2021");
 
 ?>
