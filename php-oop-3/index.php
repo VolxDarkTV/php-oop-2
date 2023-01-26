d0f00a2515d7c4067164e023276026d1c42dfefb
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-26 15:58:22 +0100

    change model

---
+++ php-oop-3/index.php
@@ -0,0 +1,162 @@
+<?php
+class Category{
+    private $name;
+    private $description;
+
+    public function __construct($name, $description){
+        $this -> setName($name); 
+        $this -> setDescription($description); 
+    }
+
+    // Name
+    public function getName(){
+        return $this -> name;
+    }
+    public function setName($name){
+        $this -> name = $name;
+    }
+    // Description
+    public function getDescription(){
+        return $this -> description;
+    }
+    public function setDescription($description){
+        $this -> description = $description;
+    }
+
+    // getHtml CATEGORY
+    public function getHtml(){
+        return "<b>Product:</b> " . $this -> getName() . "<br>"
+            . "<b>Description:</b> " . $this -> getDescription();
+    }
+}
+class Product{
+    private $name;
+    private $price;
+    private $weight;
+    private Category $category;
+
+    public function __construct($name, $price, $weight, $category){
+        $this -> setName($name);
+        $this -> setPrice($price);
+        $this -> setWeight($weight);
+        $this -> setCategory($category);
+    }
+
+    // Name
+    public function getName(){
+        return $this -> name;
+    }
+    public function setName($name){
+        $this -> name = $name;
+    }
+    // Price
+    public function getPrice(){
+        return $this -> price;
+    }
+    public function setPrice($price){
+        $this -> price = $price;
+    }
+    // Weight
+    public function getWeight(){
+        return $this -> weight;
+    }
+    public function setWeight($weight){
+        $this -> weight = $weight;
+    }
+    // Category
+    public function getCategory(){
+        return $this -> category;
+    }
+    public function setCategory($category){
+        $this -> category = $category;
+    }
+
+
+    // getHtml PRODUCT
+    public function getHtml(){
+        return $this -> getName() . "<br>"
+                . $this -> getPrice() . "<br>"
+                . $this -> getWeight() . "<br>"
+                . $this -> getCategory() -> getHtml();
+    }
+}
+
+class Food extends Product{
+    private $expDate;
+    
+    public function __construct($name, $price, $weight, $category, $expDate){
+
+        parent :: __construct($name, $price, $weight, $category);
+
+        $this -> setExpDate($expDate); 
+    }
+
+    // ExpDate
+    public function getExpDate(){
+        return $this -> expDate;
+    }
+    public function setExpDate($expDate){
+        $this -> expDate = $expDate;
+    }
+
+    // getHtml FOOD
+    public function getHtml(){
+        return parent :: getHtml() . "<br>" 
+                . $this -> getExpDate() . "<br>";
+    }
+}
+
+class Toy extends Product{
+    private $materials;
+
+    public function __construct($name, $price, $weight, $category, $materials){
+
+        parent :: __construct($name, $price, $weight, $category);
+
+        $this -> setMaterials($materials);
+    }
+
+    // Materials
+    public function getMatirials(){
+        return $this -> materials;
+    }
+    public function setMaterials($materials){
+        $this -> materials = $materials;
+    }
+
+    // getHtml TOY
+    public function getHtml(){
+        return parent :: getHtml() . "<br>" 
+                . $this -> getMatirials() . "<br>";
+    }
+}
+
+// Instance
+
+        // CATEGORY
+            // DOG
+    $dogCategory = new Category("Dog 1", "desc Dog 1");
+            // CAT
+    $catCategory = new Category("Cat 1", "desc Cat 1");
+
+        // PRODUCT
+    $product = new Product("Treats 1", 10, 200, $catCategory);
+
+        // FOOD
+    $food = new Food("Bone 1", 10, 200, $dogCategory, "2026-02-02");
+
+        // TOY
+    $toy = new Toy("Ball 1", 5, 10, $dogCategory, "plastic");
+
+// echo $dogCategory -> getHtml();
+// echo "<br>";
+// echo "<br>";
+// echo $product -> getHtml();
+// echo "<br>";
+// echo "<br>";
+echo $food -> getHtml();
+echo "<br>";
+echo "<br>";
+echo $toy -> getHtml();
+
+?>
